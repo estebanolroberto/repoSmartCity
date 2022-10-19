defda78c7107fec1b6c7047d2ae64023f7db4a12
roberr1299 <roberto.esteban@alu.uclm.es>
2022-10-19 18:17:56 +0200

    comienzo del proyecto

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var createError = require('http-errors ');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan'); 
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
+var bookmarksRouter = require('./routes/bookmarks');
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies',moviesRouter);
+app.use('/bookmarks',bookmarksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +42,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect('mongodb+srv://roberto:Calcetines@cluster0.qo7ggeh.mongodb.net/mymerndb?retryWrites=true&w=majority', { useNewUrlParser: true,
+useUnifiedTopology: true })
+.then(() => console.log('MongoDB Atlas DataBase connection successful'))
+.catch((err) => console.error(err));
 
 module.exports = app;
