2e606342d48d2c4f331a83a3810dfb68f2b477ae
roberr1299 <roberto.esteban@alu.uclm.es>
2022-10-19 18:22:16 +0200

    Instalación heroku

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan'); 
 var mongoose = require('mongoose');
 
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require('cors');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
@@ -21,6 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
